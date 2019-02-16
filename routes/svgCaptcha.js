a9990f1ac80df593c31699d215bd0513870dbb5f
Free-Yd <2890337138@qq.com>
2019-02-16 14:02:33 +0800

    添加svn验证码接口+session中间件

---
+++ routes/svgCaptcha.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var svgCaptcha = require('svg-captcha');
+var router = express.Router();
+
+/* GET svgCaptcha listing. */
+router.get('/', function (req, res, next) {
+  var captcha = svgCaptcha.create({
+    size: 5, // 验证码长度
+    ignoreChars: '0o1i', // 验证码字符中排除 0o1i
+    noise: 4, // 干扰线条的数量
+    height: 40
+  });
+  //req.session.captcha = captcha.text;
+  //req.session.captcha = captcha.text.toLowerCase(); //存session用于验证接口获取文字码
+
+  res.type('svg');
+  res.status(200).send(captcha.data);
+});
+
+module.exports = router;
